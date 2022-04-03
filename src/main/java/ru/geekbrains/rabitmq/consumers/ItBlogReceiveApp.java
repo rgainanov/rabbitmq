b2e27f8193bff1ff00dcb1f25b7495f5db347b46
Romans Gainanovs <gainanov@amazon.com>
2022-04-03 18:32:51 +0100

    ammended homework

--- src/main/java/ru/geekbrains/rabitmq/consumers/ItBlogReceiveApp.java
+++ src/main/java/ru/geekbrains/rabitmq/consumers/ItBlogReceiveApp.java
@@ -16,35 +16,49 @@ public class ItBlogReceiveApp {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
 
-        String topic;
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+        String queueName = channel.queueDeclare().getQueue();
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [*] Received '" + message + "'");
+        };
+
+        channel.basicConsume(queueName, false, deliverCallback, consumerTag -> {
+        });
+
+        String curTopic;
+        String prevTopic = null;
         System.out.println("Welcome to IT Blog");
         System.out.println("Available commands are: \n1. To set topic -> set_topic <topic>\n2. To change topic -> change_topic <new topic>");
         System.out.println("Available topics are:\n1. php\n2. java\n3. c++");
 
         while (true) {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
             String input = reader.readLine();
-            topic = input.split("\\s+", 2)[1];
+            String[] tokens = input.split("\\s+", 2);
+            String command = tokens[0];
+            curTopic = tokens[1];
 
-            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
-            String queueName = channel.queueDeclare().getQueue();
-            channel.queueBind(queueName, EXCHANGE_NAME, topic);
+            if (prevTopic == null) {
+                prevTopic = curTopic;
+            }
 
-            System.out.println(" [*] Waiting for Messages with topic: " + topic);
-
-            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-                String message = new String(delivery.getBody(), "UTF-8");
-                System.out.println(" [*] Received '" + message + "'");
-            };
-            channel.basicConsume(queueName, false, deliverCallback, consumerTag -> {
-            });
-
-
+            switch (command) {
+                case "set_topic":
+                    channel.queueBind(queueName, EXCHANGE_NAME, curTopic);
+                    break;
+                case "change_topic":
+                    channel.queueUnbind(queueName, EXCHANGE_NAME, curTopic);
+                    prevTopic = curTopic;
+                    channel.queueBind(queueName, EXCHANGE_NAME, curTopic);
+                    break;
+            }
         }
-
-
     }
 }
